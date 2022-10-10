45d53496925505bf2388b30e28156e3220a4c74b
A menina do gorro {Ana Raiany} <101668266+Ameninadogorro@users.noreply.github.com>
2022-10-10 10:39:07 -0300

    Initial Commit

---
+++ StarApi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StarApi
+//
+//  Created by Ana Raiany Guimarães Gomes on 2022-10-10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

a77dab1b5f223f3b04a80874460c6f11bc543d22
A menina do gorro {Ana Raiany} <101668266+Ameninadogorro@users.noreply.github.com>
2022-10-11 11:22:09 -0300

    add people

---
+++ StarApi/Controller/Api/ApiViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ApiViewController.swift
+//  StarApi
+//
+//  Created by Ana Raiany Guimarães Gomes on 2022-10-11.
+//
+
+import UIKit
+
+class ApiViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
